854c7fe9958660f3a37cc5116d26b15f09471919
Dave Wine <dwine@na.corp.intven.com>
2017-01-27 12:36:06 -0800

    Friday lunch work

--- Assignment 3 - DW.R
+++ Assignment 3 - DW.R
@@ -11,15 +11,37 @@ prizes <- c('Goat','Goat','Goat')
 choices <- c('Stay','Switch')
 states <- c('Closed','Closed','Closed')
 
+# Define number of games
+n <- 100
+
+
 # Functions
 ###############
 
-# Pick Another - recursively chooses another number that isn't the one passed to the function
-pick.another <-function(not.this.one){
-  x <- as.integer(runif(1,0,3))+1 
-  if(x == not.this.one) pick.another(not.this.one) else x
-  
+# Game Function
+
+game.instance <- function(pc,n){
+  #initialize counter
+  result.car <- 0
+  for (i in 1:n){
+  doors.load
+  player.guess
+  host.reveal(door.car, door.guess)
+  states[host.reveal] <- "Opened"
+  player.choose(pc)
+  player.reveal
+  # then add outcome to result set
+    if (player.prize == 'Car') result.car = result.car + 1
+  }
+result.car
 }
+
+# Pick Another - recursively chooses another number that isn't the one passed to the function
+pick.another <-function(not.this.one,all.possible) {
+  x <- as.integer(runif(1,0,all.possible)+1 )
+  if(x == not.this.one) pick.another(not.this.one,all.possible) else x
+}
+
 # Pick one door at random to hold the car
 doors.load <- function(){
   door.car <- as.integer(runif(1,0,3))+1
@@ -38,19 +60,22 @@ player.guess <- function(){
 host.reveal <-function(car,guess){
   # if the player's guess is correct, choose a goat at random
   if (guess == car) {
-    door.reveal <- pick.another(door.car) 
-    else door.reveal <- doors[-car,-guess]
+    host.reveal <- pick.another(door.car,3)
+    # otherwise choose one that isn't the car or the one the player chose
+    else host.reveal <- doors[-car,-guess]
   }
+  states[host.reveal] <- "Opened"
 }
 
-# Contestant chooses again
-player.choose <- function(){
+# Contestant chooses from remaining doors
+player.choose <- function(choice){
+  doors.left=subset(doors,states[-"Opened"]) # Can only choose from doors that are not opened
   choice <- as.integer(runif(1,0,2))+1
   if (choice == 1) {
     player.choice <- choices[1] 
     else player.choice <- choices[2] # Switch
     }
-  player.choice <- 
+  player.choice <- x
 }
 
 # Host reveals contestant door 
@@ -61,18 +86,12 @@ player.reveal <- function(){
 
 ###############
 
-# Define number of games
-n <- 100
-results <- df
+# Main Loop
 
-# Run the game
-for (i in 1:n){
-doors.load
-player.guess
-host.reveal(door.car, door.guess)
-states[host.reveal] <- "Opened"
-player.choose
-player.reveal
-}
+# Run the same number of games with the contestant 'Staying' vs. 'Switching'.
+# You could also randomize the last player choice and then sort out the wins from not, but this is equivalent and easier.
+stats.stay <-game.instance("Stay",n)
+stats.switch <- game.instance("Switch",n)
+
 
 # Plot Results
