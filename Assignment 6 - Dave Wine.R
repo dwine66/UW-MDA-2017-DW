dfe1e46d96aa695a2a0c89378ec81cd344f9f6b7
dwine66 <dwine66@gmail.com>
2017-02-20 21:10:40 -0800

    Monday PM Work

--- Assignment 6 - Dave Wine.R
+++ Assignment 6 - Dave Wine.R
@@ -4,7 +4,8 @@
 # Assignment #6: Bayes
 require ('LearnBayes')
 require ('repr')
-
+require ('ggplot2')
+require ('dplyr')
 # Probability of texting.
 # You are asked to compute the probability that the driver of a car is texting at a specific intersection.
 # Nationally the cumulative probability that a driver is texting is:
@@ -26,77 +27,94 @@ require ('repr')
 beta.par <- beta.select(list(p=0.5, x=0.1), list(p=0.75, x=0.3))
 beta.par ## The parameters of my Beta distribution
 
-options(repr.plot.width=6, repr.plot.height=5)
-triplot(beta.par, c(0, 0))
-beta.par
-# First observation: 2 texting drivers out of 20
-beta.par + c(2, 18)
-triplot(beta.par, c(2, 18))
-beta.par
-# Second observation: 4 texting drivers out of 20
-beta.par + c(6, 34)
-triplot(beta.par, c(6, 34))
-beta.par
-# Third observation: 1 texting driver out of 20
-beta.par + c(7, 53)
-triplot(beta.par, c(7, 53))
-beta.par
+## Functions
+beta.plot <- function(dist,success,failure){
+  dist + c(success, failure)
+  options(repr.plot.width=6, repr.plot.height=5)
+  triplot(dist, c(success,failure))
+  dist  
+}
+
+post.sim <- function (post, title){
+  options(repr.plot.width=8, repr.plot.height=5)
+  post.sample <- rbeta(10000, post[1], post[2])
+  par(mfrow = c(1,2))
+  quants = quantile(post.sample, c(0.05, 0.95))
+  breaks = seq(min(post.sample), max(post.sample), length.out = 41)
+  hist(post.sample, breaks = breaks, 
+       main = paste('Distribution of ',title,'samples \n with 90% HDI'),
+       xlab = 'Sample value',
+       ylab = 'Density')
+  abline(v = quants[1], lty = 3, col = 'red', lwd = 3)
+  abline(v = quants[2], lty = 3, col = 'red', lwd = 3)
+  qqnorm(post.sample)
+  par(mfrow = c(1,1))
+  quants
+}
+
+drivers.hund <- function(dist,n,title){
+  s <- 0:n
+  pred.probs <- pbetap(dist, n, s)
+  plot(s, pred.probs, type="h", 
+       main = paste('Probability distribution of the number of',title,'drivers texting at the intersection, ', as.character(n), 'trials'),
+       xlab = 'Successes')
+  discint(cbind(s, pred.probs), 0.90)
+  
+  # This creates a CDF from the PDF. It's ugly from a programming standpoint, I know.
+  dist.cdf <-rep(0,100)
+  for (i in seq(1,n,1)){
+    for (j in seq(1,i,1)) {
+      dist.cdf[i]<-dist.cdf[i]+pred.probs[j]
+    }
+  }
+  
+  plot(dist.cdf)
+  
+  list("HDI" = discint(cbind(s, pred.probs), 0.90),"CDF" = dist.cdf,"PDF"=pred.probs)
+}
+
+## Main Code
+# Plot the Prior
+beta.plot(beta.par,0,0)
+
+# Plot the successive cumulative evidence
+beta.plot(beta.par,2,18) # results for first 20
+beta.plot(beta.par,6,34) # results after 40
+beta.plot(beta.par,7,53) # results after 60
 
 # Simulate from the posterior and 
 ## compute confidence intervals
-options(repr.plot.width=8, repr.plot.height=5)
+
+# The posterior is just the number of successes relative to the total number of trials
 beta.post.par <- beta.par + c(2+4+1,18+16+19)
-post.sample <- rbeta(10000, beta.post.par[1], beta.post.par[2])
-par(mfrow = c(1,2))
-quants = quantile(post.sample, c(0.05, 0.95))
-breaks = seq(min(post.sample), max(post.sample), length.out = 41)
-hist(post.sample, breaks = breaks, 
-     main = 'Distribution of samples \n with 90% HDI',
-     xlab = 'Sample value',
-     ylab = 'Density')
-abline(v = quants[1], lty = 3, col = 'red', lwd = 3)
-abline(v = quants[2], lty = 3, col = 'red', lwd = 3)
-qqnorm(post.sample)
-par(mfrow = c(1,1))
-quants
+
+post.sim(beta.post.par, "Local Drivers")
+post.sim(beta.par,"National Average")
 
 # 7 successes out of 60 observations
 predplot(beta.post.par, 60, 7)
 
-# Now look at 100 drivers
-n <- 100
-s <- 0:n
-pred.probs <- pbetap(beta.post.par, n, s)
-plot(s, pred.probs, type="h", 
-     main = paste('Probability distribution of the number of drivers texting at the intersection, ', as.character(n), 'trials'),
-     xlab = 'Successes')
-discint(cbind(s, pred.probs), 0.90)
+# Now look at 100 drivers, both locally and nationally
+local.pred <- drivers.hund(beta.post.par,100,'Local')
+natl.pred <- drivers.hund(beta.par,100,'National')
 
-# Build CDF
-pred.cdf <-rep(0,100)
-for (i in seq(1,n,1)){
-  for (j in seq(1,i,1)) {
-    pred.cdf[i]<-pred.cdf[i]+pred.probs[j]
-  }
-}
-   
-# National Drivers 
-plot(pred.cdf)
+# Compare the CDFs
+PDF <- data.frame(local.pred$PDF,natl.pred$PDF)
+CDF <- data.frame(local.pred$CDF,natl.pred$CDF)
 
-# Now look at 100 drivers
-n <- 100
-s <- 0:n
-pred.probs <- pbetap(beta.par, n, s)
-plot(s, pred.probs, type="h", 
-     main = paste('Probability distribution of the number of drivers texting at the intersection, ', as.character(n), 'trials'),
-     xlab = 'Successes')
-discint(cbind(s, pred.probs), 0.90)
+# I tried to use ggplot for this but I could not figure it out for this plot....
+ggplot(CDF,aes(x=row.names(CDF),y=local.pred$CDF,y=natl.pred$CDF))+geom_bar()
 
-pred.cdf <-rep(0,100)
-for (i in seq(1,n,1)){
-  for (j in seq(1,i,1)) {
-    pred.cdf[i]<-pred.cdf[i]+pred.probs[j]
-  }
-}
+# Plot PDFs
+plot(local.pred$PDF,type="l",col="red",xaxt="n",yaxt="n",xlab="",ylab="",ylim=c(0,.2))
+par(new=TRUE)
+plot(natl.pred$PDF,type='l',col ="blue",xlab="",ylab="")
+title("Comparison of Local and National Drivers",xlab="Number of Drivers",ylab="Probability")
+legend("bottomright",lwd=c(2,2),col=c("red","blue"),legend=c("Local","National"))
 
-plot(pred.cdf)
+#Plot CDFs
+plot(local.pred$CDF,type="l",col="red",xlab="",ylab="")
+par(new=TRUE)
+plot(natl.pred$CDF,type='l',col ="blue",xaxt="n",yaxt="n",xlab="",ylab="")
+title("Comparison of Local and National Drivers",xlab="Number of Drivers",ylab="Cumulative Probability")
+legend("bottomright",lwd=c(2,2),col=c("red","blue"),legend=c("Local","National"))
