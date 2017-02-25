c51c43ecb73fa4adea11c5d5dab6a699ba75519d
dwine66 <dwine66@gmail.com>
2017-02-24 21:23:55 -0800

    Friday PM work
    Functional Code

--- Assignment 7 - Dave Wine.R
+++ Assignment 7 - Dave Wine.R
@@ -1,9 +1,9 @@
-# UW Data Science 350
-# Winter 2017
-# Dave Wine 8430191
-# Assignment #7: Regression
+### UW Data Science 350
+## Winter 2017
+## Dave Wine 8430191
+## Assignment #7: Regression
 
-## Libraries
+### Libraries
 require ('MASS')
 require ('repr')
 require ('ggplot2')
@@ -12,10 +12,7 @@ require ('HistData')
 require ('glmnet')
 require ('gridextra')
 
-## Read in datafile
-
-
-## Functions
+### Functions
 
 # File read function
 read.auto = function(file = 'Automobile price data _Raw_.csv'){
@@ -30,9 +27,49 @@ read.auto = function(file = 'Automobile price data _Raw_.csv'){
   auto.data[complete.cases(auto.data),]
 }
 
-####
+# Plot function
+plot.svd.reg <- function(df, k = 4){
+  require(ggplot2)
+  require(gridExtra)
+  
+  p1 <- ggplot(df) + 
+    geom_point(aes(score, resids), size = 2) + 
+    stat_smooth(aes(score, resids)) +
+    ggtitle('Residuals vs. fitted values')
+  
+  p2 <- ggplot(df, aes(resids)) +
+    geom_histogram(aes(y = ..density..)) +
+    geom_density(color = 'red', fill = 'red', alpha = 0.2) +
+    ggtitle('Histogram of residuals')
+  
+  qqnorm(df$resids)
+  
+  grid.arrange(p1, p2, ncol = 2)
+  
+  df$std.resids = sqrt((df$resids - mean(df$resids))^2)  
+  
+  p3 = ggplot(df) + 
+    geom_point(aes(score, std.resids), size = 2) + 
+    stat_smooth(aes(score, std.resids)) +
+    ggtitle('Standardized residuals vs. fitted values')
+  print(p3) 
+  
+  n = nrow(df)
+  Ybar = mean(df$price)
+  SST <- sum((df$price - Ybar)^2)
+  SSR <- sum(df$resids * df$resids)
+  SSE = SST - SSR
+  cat(paste('SSE =', as.character(SSE), '\n'))
+  cat(paste('SSR =', as.character(SSR), '\n'))
+  cat(paste('SST =', as.character(SSE + SSR), '\n'))
+  cat(paste('RMSE =', as.character(SSE/(n - 2)), '\n'))
+  
+  adjR2  <- 1.0 - (SSR/SST) * ((n - 1)/(n - k - 1))
+  cat(paste('Adjusted R^2 =', as.character(adjR2)), '\n')
+}
+###
 # Main Code
-####
+###
 
 # Read data in
 auto.data <- read.auto()
@@ -42,20 +79,17 @@ str(auto.data)
 summary(auto.data)
 
 ## Clean up dataset
-## remove first two columns
-
+# remove first two columns
 auto.data <- auto.data[,-(1:2)]
 
-
-## Transform price to log(price) based on previous work
+# Transform price to log(price) based on previous work
 auto.data$price <- log(auto.data$price)
-##
-##
-## Check on factors
-mod.mat <- model.matrix(price ~ ., data = auto.data)
-mod.mat[1:10, ]
 
-## Simple linear model
+# Check on factors
+auto.mat <- model.matrix(price ~ ., data = auto.data)
+auto.mat[1:10, ]
+
+# Simple linear model
 lm.price <- lm(price ~ ., data = auto.data)
 summary(lm.price)
 plot(lm.price)
@@ -66,22 +100,27 @@ lm.step$anova # ANOVA of the result
 summary(lm.step) # Summary of the best model
 plot(lm.step)
 
-
 ## Interaction Terms
 lm.interaction = lm(childHeight ~ mother*father, data = males.ext)
 summary(lm.interaction)
 plot(lm.interaction)
 
-
-## SVD
-M = as.matrix(males.ext[, c('mother', 'father', 'mother.sqr', 'father.sqr')])
-head(M)
+### SVD
+M = as.matrix(auto.mat)
 MTM = t(M) %*% M
-MTM
+MTM 
+# Create mSVD
 
+mSVD <- svd(MTM)
+
+mSVD$d
+
+# Note that everything below about the 10th element is 10^4 less than the largest one. So zero out
+# everything from there on down.
 cat('Compute and print the inverse singular value matrix')
-d.trim = rep(0, 4)
-d.trim[1:2] =1/ mSVD$d[1:2]
+thres <-13
+d.trim = rep(0, length(mSVD$d))
+d.trim[1:thres] =1/ mSVD$d[1:thres]
 mD = diag(d.trim)
 mD
 cat('Compute and print the pseudo inverse')
@@ -91,58 +130,19 @@ cat('Compute and print the dimensions of the matrix MTMTM')
 MTMTM = mInv %*% t(M)
 dim(MTMTM)
 
+# Find model coefficients
+b <- MTMTM %*% auto.data$price
 
+# Compute Residuals
+auto.data$score = M %*% b + mean(auto.data$price)
+auto.data$resids = auto.data$score - auto.data$price
 
-males.ext$score = M %*% b + mean(males.ext$childHeight)
-males.ext$resids = males.ext$score - males.ext$childHeight
-
+#Plot summary stats
 require(repr)
 options(repr.pmales.extlot.width=8, repr.plot.height=4)
+plot.svd.reg(auto.data, thres)
 
-plot.svd.reg <- function(df, k = 4){
-  require(ggplot2)
-  require(gridExtra)
-  
-  p1 <- ggplot(df) + 
-            geom_point(aes(score, resids), size = 2) + 
-            stat_smooth(aes(score, resids)) +
-            ggtitle('Residuals vs. fitted values')
- 
-  p2 <- ggplot(df, aes(resids)) +
-           geom_histogram(aes(y = ..density..)) +
-           geom_density(color = 'red', fill = 'red', alpha = 0.2) +
-           ggtitle('Histogram of residuals')
-
-  qqnorm(df$resids)
-    
-  grid.arrange(p1, p2, ncol = 2)
-    
-  df$std.resids = sqrt((df$resids - mean(df$resids))^2)  
-    
-  p3 = ggplot(df) + 
-            geom_point(aes(score, std.resids), size = 2) + 
-            stat_smooth(aes(score, std.resids)) +
-            ggtitle('Standardized residuals vs. fitted values')
-  print(p3) 
-    
-  n = nrow(df)
-  Ybar = mean(df$childHeight)
-  SST <- sum((df$childHeight - Ybar)^2)
-  SSR <- sum(df$resids * df$resids)
-  SSE = SST - SSR
-  cat(paste('SSE =', as.character(SSE), '\n'))
-  cat(paste('SSR =', as.character(SSR), '\n'))
-  cat(paste('SST =', as.character(SSE + SSR), '\n'))
-  cat(paste('RMSE =', as.character(SSE/(n - 2)), '\n'))
-
-  adjR2  <- 1.0 - (SSR/SST) * ((n - 1)/(n - k - 1))
-  cat(paste('Adjusted R^2 =', as.character(adjR2)), '\n')
-}
-
-plot.svd.reg(males.ext)
 ## Elastic Net Regression
-
-
 cat('Compute and print the inverse singular value matrix')
 lambda = 0.1
 d.trim = 1/ (mSVD$d + lambda)
@@ -153,13 +153,17 @@ mInv = mSVD$v %*% mD %*% t(mSVD$u)
 mInv
 MTMTM = mInv %*% t(M)
 
-males.ext$score = M %*% b2 + mean(males.ext$childHeight)
-males.ext$resids = males.ext$score - males.ext$childHeight
+b2 <- MTMTM %*% auto.data$price
 
-plot.svd.reg(males.ext)
+auto.data$score = M %*% b2 + mean(auto.data$price)
+auto.data$resids = auto.data$score - auto.data$price
+
+plot.svd.reg(auto.data,thres)
+
+## Elastic Net Regression
 
 require(glmnet)
-b = as.matrix(males.ext$childHeight)
+b = as.matrix(auto.data$price)
 mod.ridge = glmnet(M, b, family = 'gaussian', nlambda = 20, alpha = 0.0)
 plot(mod.ridge, xvar = 'lambda', label = TRUE)
 plot(mod.ridge, xvar = 'dev', label = TRUE)
@@ -172,34 +176,10 @@ mod.ridge.lasso = glmnet(M, b, family = 'gaussian', nlambda = 20, alpha = 0.5)
 plot(mod.ridge.lasso, xvar = 'lambda', label = TRUE)
 plot(mod.ridge.lasso, xvar = 'dev', label = TRUE)
 
+auto.data$score = predict(mod.ridge.lasso, newx = M)[, 15]
+auto.data$resids = auto.data$score - auto.data$price
+plot.svd.reg(auto.data)
 
-males.ext$score = predict(mod.ridge.lasso, newx = M)[, 15]
-males.ext$resids = males.ext$score - males.ext$childHeight
-
-plot.svd.reg(males.ext)
-
-
-Galton.scaled = GaltonFamilies[, c('mother', 'father', 'childHeight', 'gender')]
-Galton.scaled = mutate(Galton.scaled, 
-                       mother.sqr = mother^2, father.sqr = father^2)
-Galton.scaled[, c('mother', 'father', 'mother.sqr', 'father.sqr')] = 
-        lapply(Galton.scaled[, c('mother', 'father', 'mother.sqr', 'father.sqr')], 
-               scale)
-str(Galton.scaled)
-
-mod.mat = model.matrix(childHeight ~ ., data = Galton.scaled)
-mod.mat[1:10, ]
-
-typeof(mod.mat[, 4])
-
-b.mat = as.matrix(Galton.scaled$childHeight)
-mod.ridge.gender = glmnet(mod.mat, b.mat, family = 'gaussian', 
-                          nlambda = 20, alpha = 0.5)
-
-Galton.scaled$score = predict(mod.ridge.gender, newx = mod.mat)[, 15]
-Galton.scaled$resids = Galton.scaled$score - Galton.scaled$childHeight
-
-plot.svd.reg(Galton.scaled)
 
 
 
--- Term Project - Dave Wine_Working.R
+++ Term Project - Dave Wine_Working.R
@@ -1,12 +1,6 @@
-# UW Data Science Winter 2017
-# Dave Wine 8430191
-# Term Project - Refugees, Terrorism, and Airstrikes
-
-# Import packages
-require(ggplot2)
-require(car)
-require(plyr)
-require(dplyr)
+### UW Data Science Winter 2017
+## Dave Wine 8430191
+## Term Project - Refugees, Terrorism, and Airstrikes
 
 # Perspectives
 # 1. You are a US customs official with a long line of refugees.  You need to decide whether each is a potential
@@ -19,3 +13,50 @@ require(dplyr)
 # States.  How safe are you compared to the US citizen?
 # 6. You are a policy wonk in the US government, trying to decide whether to add additional screening to 
 # a refugee admission process.
+
+### Import packages
+require(ggplot2)
+require(gridExtra)
+require(car)
+require(plyr)
+require(dplyr)
+require(LearnBayes)
+
+### Functions
+
+# File read function
+read.datafile = function(file = 'Automobile price data _Raw_.csv',skip=0){
+  datafile.data <- read.csv(file, header=TRUE, stringsAsFactors=FALSE,skip=skip)
+  
+#  numcols <- c('bore','stroke','horsepower','price','peak.rpm')
+#  gtd.data[, numcols]<-lapply(auto.data[,numcols], as.numeric)
+  
+#  factcols <- c('make','fuel.type','aspiration','num.of.doors','drive.wheels','engine.location','engine.type','num.of.cylinders','fuel.system','body.style')
+#  gtd.data[, factcols]<-lapply(auto.data[,factcols], as.factor)
+  
+#  gtd.data[complete.cases(auto.data),]
+}
+
+### Main Code
+
+## Read data in
+# Set base working directory
+wd <- "C:/Users/Dave/Google Drive/UW Data Science/2017 Q1 - MDA/Term Project"
+setwd(wd)
+
+# Refugee flows
+ref.data <-read.datafile("UNHCR_refugee_flows.csv",skip=5)
+
+# Terrorist Nationality
+tnat.data <-read.datafile("AN_Terrorist_Nationality.csv")
+
+# GTD database
+setwd(paste(wd,"/GTD_0616dist",sep=""))
+gtd.data <- read.datafile("globalterrorismdb_0616dist.csv")
+
+#clean up database
+
+# View dataset and summary statistics
+str(ref.data)
+str(tnat.data)
+str(gtd.data)
