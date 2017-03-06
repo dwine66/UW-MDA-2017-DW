8d520adede4236b82b8768d56080b811f60d5e6a
dwine66 <dwine66@gmail.com>
2017-03-06 06:12:29 -0800

    Monday AM work

--- Term Project - Dave Wine_Working.R
+++ Term Project - Dave Wine_Working.R
@@ -26,7 +26,7 @@ require(treemap)
 require(repr)
 
 ### Functions
-
+# Bayesian Beta Plot
 beta.plot <- function(dist,success,failure){
   dist + c(success, failure)
   options(repr.plot.width=6, repr.plot.height=5)
@@ -34,6 +34,7 @@ beta.plot <- function(dist,success,failure){
   dist  
 }
 
+# Bayesian Posterior Simulation
 post.sim <- function (post, title){
   options(repr.plot.width=8, repr.plot.height=5)
   post.sample <- rbeta(10000, post[1], post[2])
@@ -51,8 +52,9 @@ post.sim <- function (post, title){
   
   list(quants,paste("Mean: ",mean(post.sample)),paste("Median: ",median(post.sample)))
 }
+
 # Predicted distribution of n more trials
-drivers.hund <- function(dist,n,title){
+distpred <- function(dist,n,title){
   s <- 0:n
   pred.probs <- pbetap(dist, n, s)
   plot(s, pred.probs, type="h", 
@@ -73,8 +75,17 @@ drivers.hund <- function(dist,n,title){
   list("HDI" = discint(cbind(s, pred.probs), 0.90),"CDF" = dist.cdf,"PDF"=pred.probs)
 }
 
-# Map
-mapplot <-function(dataset,title,xr=c(-180,180),yr=c(-90,90)){
+# Distribution Plot
+dist.plot <- function(data1,data2,Name,dname1,dname2){
+  plot(data1,type="l",col="red",xaxt="n",yaxt="n",xlab="",ylab="") #,ylim=c(0,.2))
+  par(new=TRUE)
+  plot(data2,type='l',col ="blue",xlab="",ylab="")
+  title(Name,xlab=dname1,ylab=dname2)
+  legend("topright",lwd=c(2,2),col=c("red","blue"),legend=c(dname1,dname2))
+}
+
+# World Map Plotting
+map.plot <-function(dataset,title,xr=c(-180,180),yr=c(-90,90)){
   par(mai=c(0,0,.25,0))
   newmap <- getMap(resolution = "low")
   plot(newmap,main=title,xlim=xr,ylim=yr)
@@ -103,7 +114,9 @@ read.datafile = function(file = 'Automobile price data _Raw_.csv',skip=0){
 #  gtd.data[complete.cases(auto.data),]
 }
 
+###
 ### Main Code
+###
 
 ## Read data in
 # Set base working directory
@@ -112,6 +125,7 @@ setwd(wd)
 
 # Refugee flows
 ref.data <-read.datafile("UNHCR_refugee_flows.csv",skip=5)
+ref2.data <-read.datafile("20170305 UNHCR Refugee Data_US.csv",skip=4)
 
 # Terrorist Nationality
 tnat.data <-read.datafile("AN_Terrorist_Nationality.csv")
@@ -135,12 +149,12 @@ gtd.data <-rbind.fill(gtd.data,gtd.1993)
 colnames(gtd.data)[2] <-"Year"
 colnames(ref.data)[1] <-"Destination"
 colnames(ref.data)[2] <-"Origin"
+colnames(ref2.data)[2] <-"Destination"
 colnames(tnat.data)[1] <-"Origin"
 
 # Turn NA's to zero where appropriate
 #ref.data <- gsub("*","NA",ref.data)
 ref.data[is.na(ref.data)] <-0
-
 
 # Add summary data where appropriate 
 ref.data$Total <- rowSums(ref.data[,4:15]) #through 2012
@@ -154,6 +168,7 @@ str(crime.data)
 str(gtd.data)
 
 # Subset most relevant data
+# INT_IDEO is the closest thing to country data in the GTD.
 gtd.sub <- select(gtd.data,Year,country,country_txt,longitude,latitude,attacktype1_txt,targtype1, +
                     targtype1_txt,success,nkill,nkillus,nwound,nwoundus,INT_IDEO)
 
@@ -161,56 +176,56 @@ gtd.sub.fatal <- filter(gtd.sub,nkill!="NA")
 gtd.sub.fatal <- filter(gtd.sub,nkill>0)
 gtd.sub.fatal$nkill <- log10(gtd.sub.fatal$nkill)
 
+# Attacks that killed more than 1000 people
 gtd.data.scary <- filter(gtd.data,nkill>1000)
+
+# Fractional fatalities (more terrorists than victims)
 gtd.data.low <- filter(gtd.data,nkill<1)
 gtd.data.low <- filter(gtd.data.low,nkill>0)
+
 # US murder data
 murders.sub <- select(filter(crime.data,Year>1969),Year,Murder) 
 murders.prior <- filter(murders.sub,Year<1991)
 
 ## Visualize basic data
 attacks.US<-filter(gtd.sub,country_txt=="United States")
+
 # Domestic vs International
-attacks.US.dom <-filter(gtd.sub,country_txt=="United States",INT_IDEO==0)
+attacks.US.dom <- filter(gtd.sub,country_txt=="United States",INT_IDEO==0)
 attacks.US.int <- filter(gtd.sub,country_txt=="United States",INT_IDEO==1)
 attacks.US.unk <- filter(gtd.sub,country_txt=="United States",INT_IDEO==-9)
 
-
+# Plot treemap of primary targets
 types.US <- attacks.US %>% dplyr::count(targtype1_txt) 
 types.US <- types.US[order(types.US$n),]
-treemap(types.US,"targtype1_txt","n")
-
-atype.ww <- gtd.sub %>% dplyr::count(attacktype1_txt) 
-atype.ww <- atype.ww[order(atype.ww$n),]
-
-treemap(atype.ww,"attacktype1_txt","n",type="index",palette="Blues",lowerbound.cex.labels = 0,title="Attack type (Worldwide)")
+treemap(types.US,"targtype1_txt","n",type="index",palette="Reds",lowerbound.cex.labels = 0 ,title="Target type (Worldwide)")
 
 hist(attacks.US.int$Year, breaks=46,main = "Foreign Terrorist attacks in the US")
 hist(attacks.US.dom$Year, breaks=46,main = "Domestic Terrorist attacks in the US")
 
-# Bar plot of attack types
-par(mfcol=c(1,1))
-barplot(types.US$n,names.arg=types.US$targtype1_txt,horiz=TRUE)
-
-
+# Target Types
 ggplot(attacks.US, aes(x=reorder(targtype1_txt,targtype1_txt,function(x) -length(x)))) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  ggtitle('Bar chart of Terrorist Attack Type in US')
+  ggtitle('Bar chart of Terrorist Target Type: US')
 
 ggplot(gtd.data, aes(x=reorder(targtype1_txt,targtype1_txt,function(x) -length(x)))) +
   geom_bar() +
   theme(axis.text.x = element_text(angle = 90, hjust = 1)) +
-  ggtitle('Bar chart of Terrorist Attack Type ww')
+  ggtitle('Bar chart of Terrorist Target Type: Worldwide')
 
 ggplot(gtd.sub.fatal, aes(x = factor(attacktype1_txt), y = nkill)) + geom_boxplot() +
-theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
+  theme(axis.text.x = element_text(angle = 45, hjust = 1)) +
   ggtitle('Boxplot of Terrorism Fatalities')
 
+# Plot treemap of attack methodologies worldwide
+atype.ww <- gtd.sub %>% dplyr::count(attacktype1_txt) 
+atype.ww <- atype.ww[order(atype.ww$n),]
+treemap(atype.ww,"attacktype1_txt","n",type="index",palette="Blues",lowerbound.cex.labels = 0 ,title="Attack type (Worldwide)")
 
 # Map stuff
 
-mapplot(gtd.data,"Terrorist Events - Worldwide")
+map.plot(gtd.data,"Terrorist Events - Worldwide")
 
 par(mai=c(0,0,.25,0))
 newmap <- getMap(resolution = "low")
@@ -219,15 +234,16 @@ points(attacks.US.int$longitude, attacks.US.int$latitude,col='red',pch=1)
 points(attacks.US.dom$longitude, attacks.US.dom$latitude,col='blue',pch=1)
 points(attacks.US.unk$longitude, attacks.US.unk$latitude,col='green',pch=1)
 
-mapplot(attacks.US,"Terrorist Events in the US",c(-150,-75),c(15,65))
+map.plot(attacks.US,"Terrorist Events in the US",c(-150,-75),c(15,65))
 
 # by year
 hist(gtd.data$Year,breaks=46,main="worldwide attacks by year")
 
 ## Bayesian Analysis
 
-# Filter US attacks by international origin (exclude domestic) and if it caused a fatality
+# Bayesian #1: Analysis of attacks before and after 1991
 
+# Filter US attacks by international origin (exclude domestic) and if it caused a fatality
 tdeaths.US <- select(attacks.US.int,Year,nkillus)
 tdeaths.US <- filter(tdeaths.US,nkillus>=0)
 tdeaths.US <- tdeaths.US %>% group_by(Year) %>% dplyr::summarise(tfatal=sum(nkillus))
@@ -252,7 +268,6 @@ barplot(tnat.data$Murders,names=tnat.data$Country)
 #beta.par <- beta.select(list(p=0.5, x=0.1), list(p=0.75, x=.3))
 beta.par <-c(15,4)
 beta.par ## The parameters of my Beta distribution
-
 
 # Plot the Prior
 beta.plot(beta.par,0,0)
@@ -279,32 +294,20 @@ post.sim.natl
 # 7 successes out of 60 observations
 predplot(beta.post.par, 25, 2)
 
-# Now look at 100 drivers, both locally and nationally
-local.pred <- drivers.hund(beta.post.par,100,'Local')
-natl.pred <- drivers.hund(beta.par,100,'National')
+# Now look at the next 100 years
+post1990.pred <- distpred(beta.post.par,100,'Local')
+pre1990.pred <- distpred(beta.par,100,'National')
 
 # Compare the CDFs
-PDF <- data.frame(local.pred$PDF,natl.pred$PDF)
-CDF <- data.frame(local.pred$CDF,natl.pred$CDF)
+PDF <- data.frame(post1990.pred$PDF,pre1990.pred$PDF)
+CDF <- data.frame(post1990.pred$CDF,pre1900.pred$CDF)
 
-# I tried to use ggplot for this but I could not figure it out for this plot....
-#ggplot(CDF,aes(x=row.names(CDF),y=local.pred$CDF,y=natl.pred$CDF))+geom_bar()
+# Plot PDFs and CDFs
+dist.plot(post1990.pred$PDF,pre1990.pred$PDF,"PDF of Pre- and Post 1990","Post-1990","Pre-1990")
+dist.plot(post1990.pred$CDF,pre1990.pred$CDF,"PDF of Pre- and Post 1990","Post-1990","Pre-1990")
 
-# Plot PDFs
-plot(local.pred$PDF,type="l",col="red",xaxt="n",yaxt="n",xlab="",ylab="",ylim=c(0,.2))
-par(new=TRUE)
-plot(natl.pred$PDF,type='l',col ="blue",xlab="",ylab="")
-title("Comparison of Local and National Drivers",xlab="Number of Drivers",ylab="Probability")
-legend("topright",lwd=c(2,2),col=c("red","blue"),legend=c("Local","National"))
-
-#Plot CDFs
-plot(local.pred$CDF,type="l",col="red",xlab="",ylab="")
-par(new=TRUE)
-plot(natl.pred$CDF,type='l',col ="blue",xaxt="n",yaxt="n",xlab="",ylab="")
-title("Comparison of Local and National Drivers",xlab="Number of Drivers",ylab="Cumulative Probability")
-legend("bottomright",lwd=c(2,2),col=c("red","blue"),legend=c("Local","National"))
-
-## Refugee Data
+# Bayesian #2: Refugee Correlation Study
+# Filter to refugees that entered the US
 ref.us <-filter(ref.data,Destination=="United States",Population.type!="Returned refugees")
 ref.us.ter <-inner_join(ref.us,tnat.data,by="Origin")
 ref.us.ter <- filter(ref.us.ter,Terrorists!=0)
@@ -333,6 +336,7 @@ post.sim.ter
 
 options(repr.plot.width = 8,repr.plot.height = 4)
 
+# Bayesian #3: Countries with known terrorists
 ref.us.tot=colSums(ref.us[4:16])
 ref.us.prior=sum(ref.us.tot[4:9])
 ref.us.post=sum(ref.us.tot[10:13])
@@ -346,9 +350,9 @@ at.US.post <- filter(at.US.tot,Year>2005)
 
 US.comb.prior <- cbind(at.US.prior,ref.us.prior[1:6])
 
-# Was there a terrorist in the refugee population (a=no, b=yes)
+# How many terrorists were in the refugee population (a=terrorists, b=non-terrorists)
 #beta.par <- beta.select(list(p=0.5, x=0.1), list(p=0.75, x=.3))
-beta.ter <- c(80,4723932-80)
+beta.ter <- c(80,ref.us.prior-80)
 beta.plot(beta.ter,0,0)
 
 beta.post.ter <- beta.ter+c(21,ref.us.post)
@@ -360,23 +364,18 @@ post.sim.natl
 
 predplot(beta.post.ter, 21, ref.us.post)
 
-# Now look at 100 drivers, both locally and nationally
-local.pred <- drivers.hund(beta.post.ter,10000,'Local')
-natl.pred <- drivers.hund(beta.ter,10000,'National')
+# Now look at the next 10000 refugees
+post2005.pred <- distpred(beta.post.ter,10000,'Post-2005')
+pre2005.pred <- distpred(beta.ter,10000,'Pre-2005')
+#  Was the pre data a good predictor?
 
 # Compare the CDFs
-PDF <- data.frame(local.pred$PDF,natl.pred$PDF)
-CDF <- data.frame(local.pred$CDF,natl.pred$CDF)
-# Plot PDFs
-plot(local.pred$PDF,type="l",col="red",xaxt="n",yaxt="n",xlab="",ylab="",ylim=c(0,.2))
-par(new=TRUE)
-plot(natl.pred$PDF,type='l',col ="blue",xlab="",ylab="")
-title("Comparison of Local and National Drivers",xlab="Number of Drivers",ylab="Probability")
-legend("topright",lwd=c(2,2),col=c("red","blue"),legend=c("Local","National"))
+ref2005.PDF <- data.frame(post2005.pred$PDF,pre2005.pred$PDF)
+ref2005.CDF <- data.frame(post2005.pred$CDF,pre2005.pred$CDF)
 
-#Plot CDFs
-plot(local.pred$CDF,type="l",col="red",xlab="",ylab="")
-par(new=TRUE)
-plot(natl.pred$CDF,type='l',col ="blue",xaxt="n",yaxt="n",xlab="",ylab="")
-title("Comparison of Local and National Drivers",xlab="Number of Drivers",ylab="Cumulative Probability")
-legend("bottomright",lwd=c(2,2),col=c("red","blue"),legend=c("Local","National"))
+# Plot PDFs
+dist.plot(post2005.pred$PDF,pre2005.pred$PDF,"PDF of Pre- and Post 2005","Post-2005","Pre-2005")
+dist.plot(post2005.pred$CDF,pre2005.pred$CDF,"CDF of Pre- and Post 2005","Post-2005","Pre-2005")
+
+
+#Do a classic disease analysis based on these numbers
